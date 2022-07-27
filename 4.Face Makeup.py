845f4f1fcea1db8b914e714a63601cb7410041ca
Arman Golbidi <drarmangolbidi@gmail.com>
2022-07-27 23:52:41 +0430

    Update 4.Face Makeup.py

--- 4.Face Makeup.py
+++ 4.Face Makeup.py
@@ -1,27 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import face_recognition
 from PIL import Image, ImageDraw
 
-
-# In[2]:
-
-
-face_image = face_recognition.load_image_file('picture.jpg')
-
+face_image = face_recognition.load_image_file('Arman.jpg')
 
 face_landmarks_list =  face_recognition.face_landmarks(face_image)
 
-
 print(face_landmarks_list)
-
-
-# In[3]:
-
 
 for face_landmarks in face_landmarks_list:
     
@@ -39,23 +23,12 @@ for face_landmarks in face_landmarks_list:
     d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
     d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
 
-
-#     d.polygon(face_landmarks['left_eye'], fill=( , , , ))
-#     d.polygon(face_landmarks['right_eye'], fill=( , , , ))
-
-    
     d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
     d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
 
-
-  
 pil_image.show()
 
-
 pil_image.save("makeup.jpg")
-
-
-# In[ ]:
 
 
 
